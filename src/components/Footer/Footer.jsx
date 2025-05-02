5c12748f18416a85cc3b7a0eda5b66b1081def6d
navjot kaur <navu77196@gmail.com>
2025-05-02 15:57:56 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+    const [newDate, setNewDate] = useState('');
+    useEffect(() => {
+        let todayDate = new Date();
+        let todayYear = todayDate.getFullYear();
+        setNewDate([todayYear])
+    }, []);
+
+  return (
+    <footer className='bg-body-tertiary' id='footer'>
+        <p className='pt-3 m-0 text-center'>copyright &copy; Calgary veg, 2021-{newDate}</p>
+       
+    </footer>
+  )
+}
+
+export default Footer;
