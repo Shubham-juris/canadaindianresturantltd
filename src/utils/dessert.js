b3aafab2a70d991fb9bab5e97865332eeb2cf2dc
JurisNexaIt <shubham.developerjuris@gmail.com>
2025-05-16 10:03:57 +0530

    Update dessert.js

--- src/utils/dessert.js
+++ src/utils/dessert.js
@@ -3,19 +3,19 @@ const dessert = [
         id: 1,
         name: 'Gulab Jamun',
         description: 'deep-fried milk solids soaked in rose-flavored sugar syrup',
-        price: '5.99' // CAD
+        price: '5.99$' // CAD
     },
     {
         id: 2,
         name: 'Rasmalai',
         description: 'soft paneer balls soaked in sweet, saffron-infused milk',
-        price: '6.50' // CAD
+        price: '6.50$' // CAD
     },
     {
         id: 3,
         name: 'Gajar Halwa',
         description: 'grated carrots cooked in ghee with milk, sugar, and cardamom',
-        price: '6.75' // CAD
+        price: '6.75$' // CAD
     }
 ];
 
