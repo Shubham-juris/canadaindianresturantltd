a82fce1c3e78258e61ea00e7080ccb8612fd63d2
JurisNexaIt <shubham.developerjuris@gmail.com>
2025-05-16 10:07:08 +0530

    Update food.js

--- src/utils/food.js
+++ src/utils/food.js
@@ -2,27 +2,27 @@ const food = [
     {
         id: 1,
         name: 'Paneer Butter Masala',
-        price: '13.99' 
+        price: '13.99$' 
     },
     {
         id: 2,
         name: 'Masala Dosa',
-        price: '10.50' 
+        price: '10.50$' 
     },
     {
         id: 3,
         name: 'Chole Bhature',
-        price: '11.25' 
+        price: '11.25$' 
     },
     {
         id: 4,
         name: 'Veg Biryani',
-        price: '12.75'
+        price: '12.75$'
     },
     {
         id: 5,
         name: 'Aloo Paratha with Curd',
-        price: '9.99' 
+        price: '9.99$' 
     }
 ];
 
