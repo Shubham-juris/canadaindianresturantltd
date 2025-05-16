e911593e1cc3be2cb84100d069e5218bcbb24e6c
JurisNexaIt <shubham.developerjuris@gmail.com>
2025-05-16 10:16:40 +0530

    Update lunch.js

--- src/utils/lunch.js
+++ src/utils/lunch.js
@@ -3,19 +3,19 @@ const lunch = [
         id: 1,
         name: 'Paneer Butter Masala with Naan',
         description: 'cottage cheese cubes in rich tomato-butter gravy served with soft naan bread',
-        price: '13.99' 
+        price: '13.99$' 
     },
     {
         id: 2,
         name: 'Chole Bhature',
         description: 'spiced chickpeas curry served with deep-fried fluffy bread',
-        price: '11.75' 
+        price: '11.75$' 
     },
     {
         id: 3,
         name: 'Vegetable Biryani with Raita',
         description: 'fragrant basmati rice cooked with vegetables and aromatic spices, served with yogurt dip',
-        price: '12.50' 
+        price: '12.50$' 
     }
 ];
 
