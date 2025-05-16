6542c12787a523794cf602cb1c1c465569653d9c
JurisNexaIt <shubham.developerjuris@gmail.com>
2025-05-16 10:05:17 +0530

    Update dinner.js

--- src/utils/dinner.js
+++ src/utils/dinner.js
@@ -3,19 +3,19 @@ const dinner = [
         id: 1,
         name: 'Butter Paneer with Rice',
         description: 'tandoori Paneer in creamy tomato gravy served with basmati rice',
-        price: '16.99' // CAD
+        price: '16.99$' // CAD
     },
     {
         id: 2,
         name: 'Dal Tadka with Jeera Rice',
         description: 'yellow lentils tempered with garlic and spices, served with cumin-flavored rice',
-        price: '13.25' // CAD
+        price: '13.25$' // CAD
     },
     {
         id: 3,
         name: 'Palak Paneer with Roti',
         description: 'spinach curry with Indian cottage cheese served with whole wheat roti',
-        price: '14.50' // CAD
+        price: '14.50$' // CAD
     }
 ];
 
