a75ae5806498572701adc12b30c12d748739bcc4
JurisNexaIt <shubham.developerjuris@gmail.com>
2025-05-16 10:06:36 +0530

    Update drinks.js

--- src/utils/drinks.js
+++ src/utils/drinks.js
@@ -2,27 +2,27 @@ const drinks = [
     {
         id: 1,
         name: 'Masala Chai',
-        price: '2.99' // CAD
+        price: '2.99$' // CAD
     },
     {
         id: 2,
         name: 'Sweet Lassi',
-        price: '3.49' // CAD
+        price: '3.49$' // CAD
     },
     {
         id: 3,
         name: 'Mango Lassi',
-        price: '3.99' // CAD
+        price: '3.99$' // CAD
     },
     {
         id: 4,
         name: 'Filter Coffee',
-        price: '2.75' // CAD
+        price: '2.75$' // CAD
     },
     {
         id: 5,
         name: 'Nimbu Pani (Lemonade)',
-        price: '2.50' // CAD
+        price: '2.50$' // CAD
     }
 ];
 
